75577bcb3cfb5ac6b61abf72bca92787e371b557
jenniferk2019 <51275153+jenniferk2019@users.noreply.github.com>
2019-07-03 09:42:53 -0400

    new javacript functions updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,53 @@
-var yourNumber =
-    document.getElementById('yourNumber');
-yourScore.innerHTML = 0;
 
-var crystalNumber =
-    document.getElementById('crystalNumber');
-targetScore.innerHTML = getRandomValue(10); 
-// is the ^ getRandomValue properly used?? to generate random numbers by ten??
+
+var yourScore=0
+
+function crystalMath () {
+    if ($("#moonCrystal").on("click", function() {
+        var moonCrystal=Math.floor(Math.random() * 12 +1);
+        console.log(moonCrystal)
+    })) {
+        yourScore = yourScore + moonCrystal;
+    } 
+    else if ($("#princessCrystal").on("click", function() {
+        var princessCrystal=Math.floor(Math.random() * 12 +1);
+        console.log(princessCrystal)
+    })) {
+        yourScore = yourScore + princessCrystal;
+    }
+    else if ($("#starCrystal").on("click", function() {
+        var starCrystal=Math.floor(Math.random() * 12 +1);
+        console.log(starCrystal)
+    })) {
+        yourScore = yourScore + starCrystal;
+    }
+    else if ($("#cosmicCrystal").on("click", function() {
+        var cosmicCrystal=Math.floor(Math.random() * 12 +1);
+        console.log(cosmicCrystal)
+    })) {
+        yourScore = yourScore + cosmicCrystal;
+    }document.getElementById('crystalNumber').textContent = crystalNumber;
+
+}
+crystalMath()
+
+function random() {
+var crystalNumber = Math.floor(Math.random() * 120 + 19);
+document.getElementById('crystalNumber').textContent = crystalNumber;
+console.log(crystalNumber)
+}
+
+random()
+
+var yourScore = 0
 
 //*******************************************************/
 
-function addToScore(val) {
-    var numberToAdd = parseInt(val);
-    var currentScore = parseInt(yourScore.innerHTML);
-    yourScore.innerHTML = numberToAdd + currentScore;
-}
+// function addToScore(val) {
+//     var numberToAdd = parseInt(val);
+//     var currentScore = parseInt(yourScore.innerHTML);
+//     yourScore.innerHTML = numberToAdd + currentScore;
+// }
 
 // *****************************************************/
 
@@ -36,32 +70,34 @@ console.log(cosmicCrystal.value);
 //********************************************************/
 
 // add an [event listener] to elements
+// jQuery to shorten the below javascript??
 
-moonCrystal.addEventListener('click', function () {
-    //this refers to Moon Crystal
-    addToScore(this.value);
-    checkPlayerScore();
-});
 
-princessCrystal.addEventListener('click', function () {
-    // this refers to Princess Crystal
-    addToScore(this.value);
-    checkPlayerScore();
-});
+// moonCrystal.addEventListener('click', function () {
+//     //this refers to Moon Crystal
+//     addToScore(this.value);
+//     checkPlayerScore();
+// });
 
-starCrystal.addEventListener('click', function () {
-    // this refers to Star Crystal
-    addToScore(this.value);
-    checkPlayerScore();
-});
+// princessCrystal.addEventListener('click', function () {
+//     // this refers to Princess Crystal
+//     addToScore(this.value);
+//     checkPlayerScore();
+// });
 
-cosmicCrystal.addEventListener('click', function () {
-    // this refers to Cosmic Crystal
-    addToScore(this.value);
-    checkPlayerScore();
-});
+// starCrystal.addEventListener('click', function () {
+//     // this refers to Star Crystal
+//     addToScore(this.value);
+//     checkPlayerScore();
+// });
+
+// cosmicCrystal.addEventListener('click', function () {
+//     // this refers to Cosmic Crystal
+//     addToScore(this.value);
+//     checkPlayerScore();
+// });
 
 // *********************************************************/
 
-var sum = parseInt(moonCrystal.value) + parseInt(princessCrystal.value) + parseInt(starCrystal.value) + parseInt(cosmicCrystal.value);
-console.log (sum);
+// var sum = parseInt(moonCrystal.value) + parseInt(princessCrystal.value) + parseInt(starCrystal.value) + parseInt(cosmicCrystal.value);
+// console.log (sum);
